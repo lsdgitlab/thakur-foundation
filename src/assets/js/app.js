da1b82493904e3b774d28eae33a1b1c6deebc4e6
lsdigitalab <ravindra.dhadave@lsdigital.com>
2022-09-12 16:40:39 +0530

    initail the project

---
+++ src/assets/js/app.js
@@ -0,0 +1,35 @@
+import style from '../sass/main.scss';
+// import style1 from '../css/main.css';
+// import component from './component.js';
+// import componentCircle from './circle.js'
+
+// // Edited
+// import editcode from "../js/edited.js";
+
+// Progress
+import progrssStep from "../js/progress-steps.js";
+
+
+
+
+
+
+
+
+
+
+
+
+// const heading = "Welcome to Digiserve";
+// document.querySelector('h2').innerText = heading 
+
+// console.log("check App JS");
+// cube
+
+// // leetcode
+// document.body.append(leetCode())
+
+// leetcode
+// document.body.append(form())
+
+// circle
