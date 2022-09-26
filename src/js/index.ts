cefcc2601617420fc27bd7dcf4c991f48c63f25c
Shubham <shubham.belge@lsdigital.com>
2022-09-26 10:24:29 +0530

    biography page and index page is done.

--- src/js/index.ts
+++ src/js/index.ts
@@ -46,27 +46,55 @@ $(window).on('scroll',function () {
   }
 });
 
-if ($('#bannerCarousel').length) {
-  let bannerCarousel = $("#bannerCarousel").owlCarousel({
-    margin:20,
-    items: 1,
-    dots:false,
-    nav:true,
-    navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
-    autoplayTimeout: 5000,
-    loop:true,
-    smartSpeed: 2000,
-  })
-};
+// if ($('#bannerCarousel').length) {
+//   let bannerCarousel = $("#bannerCarousel").owlCarousel({
+//     margin:20,
+//     items: 1,
+//     dots:false,
+//     nav:true,
+//     navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+//     autoplayTimeout: 5000,
+//     loop:true,
+//     smartSpeed: 2000,
+//   })
+// };
 if ($('#articlesCarousel').length) {
   let articlesCarousel = $("#articlesCarousel").owlCarousel({
     margin:20,
-    items: 4,
+    // items: 4,
     dots:false,
     nav:true,
-    navText: ["<img src='./../img/arrow.svg'>","<img src='./../img/arrow.svg'>"],
+    navText: ["<img src='./img/arrow.svg'>","<img src='./img/arrow.svg'>"],
     autoplayTimeout: 5000,
     loop:true,
     smartSpeed: 2000,
+    responsive:{
+      0:{
+          items:1,
+          // nav:true
+      },
+      992:{
+        items:4,
+        // nav:true
+      },
+    }
   })
 };
+$(".toggle_icon").click(function(){
+  $('.mobile-menu').addClass('open animate__fadeInLeft')
+})
+// $('.mobile-menu').click((e)=>{
+//   console.log("click");
+//   console.log($(e.target).closest('.link-cotainer'));
+  
+//   if (!$(e.target).closest('.link-cotainer').length){
+//     $(".mobile-menu").toggleClass('open')
+//   }
+// });
+$(".close-menu").click(function(){
+  console.log("clis")
+  $('.mobile-menu').addClass('animate__fadeOutLeft')
+  setTimeout(() => {
+    $('.mobile-menu').removeClass('open animate__fadeOutLeft animate__fadeInLeft')
+  }, 900);
+})
